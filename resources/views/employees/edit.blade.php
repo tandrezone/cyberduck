789e7e0824e64228eba7d85fb1a843d93a7d6296
Tiago Andre <tandrezone@gmail.com>
2021-03-11 15:26:30 +0000

    added views for employee

---
+++ resources/views/employees/edit.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Employee</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('employees.index') }}" title="Go back"> <i class="fas fa-backward "></i> </a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('employees.update', $employee->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>First Name:</strong>
+                    <input type="text" name="first_name" value="{{ $employee->first_name }}" class="form-control" placeholder="First Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Last Name:</strong>
+                    <input type="text" name="last_name" value="{{ $employee->last_name }}" class="form-control" placeholder="Last Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Email:</strong>
+                    <textarea class="form-control" style="height:50px" name="email"
+                              placeholder="Email">{{ $employee->email }}</textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Phone:</strong>
+                    <input type="text" name="phone" class="form-control" placeholder="Phone"
+                           value="{{ $employee->phone }}">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Company:</strong>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+
+    </form>
+@endsection
---
+++ resources/views/employees/index.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Employees</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('employees.create') }}" title="Create a employee"> <i class="fas fa-plus-circle"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <table class="table table-bordered table-responsive-lg">
+        <tr>
+            <th>Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Company</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($employees as $employee)
+            <tr>
+                <td>{{ $employee->id}}</td>
+                <td>{{ $employee->first_name }}</td>
+                <td>{{ $employee->last_name }}</td>
+                <td>{{ $employee->email }}</td>
+                <td>{{ $employee->phone }}</td>
+                <td>{{ $employee->company }}</td>
+                <td>
+                    <form action="{{ route('employees.destroy', $employee->id) }}" method="POST">
+
+                        <a href="{{ route('employees.show', $employee->id) }}" title="show">
+                            <i class="fas fa-eye text-success  fa-lg"></i>
+                        </a>
+
+                        <a href="{{ route('employees.edit', $employee->id) }}">
+                            <i class="fas fa-edit  fa-lg"></i>
+
+                        </a>
+
+                        @csrf
+                        @method('DELETE')
+
+                        <button type="submit" title="delete" style="border: none; background-color:transparent;">
+                            <i class="fas fa-trash fa-lg text-danger"></i>
+
+                        </button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+
+@endsection
