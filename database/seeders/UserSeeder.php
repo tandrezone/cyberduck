31810a84a1190466e2de21850a048a39729dea1c
Tiago Andre <tandrezone@gmail.com>
2021-03-11 15:46:00 +0000

    Fix password on seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -17,7 +18,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'Admin',
             'email' => 'admin@admin.com',
-            'password' => 'password',
+            'password' => Hash::make('password'),
         ]);
     }
 }
